c5fdfa0468520e5fa50af935c06a5c23353ffcaa
Dragan Draganov <dragan.mdraganov@yahoo.com>
2015-10-23 14:08:32 +0300

    Refactored identity models

---
+++ FamousQuoteQuiz.Data/FamousQuizDbContext.cs
@@ -0,0 +1,23 @@
+﻿using FamousQuoteQuiz.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamousQuoteQuiz.Data
+{
+    public class FamousQuizDbContext : IdentityDbContext<User>
+    {
+        public FamousQuizDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static FamousQuizDbContext Create()
+        {
+            return new FamousQuizDbContext();
+        }
+    }
+}
