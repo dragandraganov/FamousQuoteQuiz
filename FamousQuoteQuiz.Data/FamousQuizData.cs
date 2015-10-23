0501bd07a3cd844f5b68f13614e74b7e0cacabc5
Dragan Draganov <dragan.mdraganov@yahoo.com>
2015-10-23 14:19:59 +0300

    Added Unit of Work design pattern

---
+++ FamousQuoteQuiz.Data/FamousQuizData.cs
@@ -0,0 +1,66 @@
+﻿using FamousQuoteQuiz.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamousQuoteQuiz.Data
+{
+    public class FamousQuizData : IFamousQuizData
+    {
+        private readonly DbContext context;
+
+        private readonly Dictionary<Type, object> repositories = new Dictionary<Type, object>();
+        public FamousQuizData(DbContext context)
+        {
+            this.context = context;
+        }
+        public DbContext Context
+        {
+            get
+            {
+                return this.context;
+            }
+        }
+
+        public IRepository<Models.User> Users
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.context != null)
+                {
+                    this.context.Dispose();
+                }
+            }
+        }
+
+        public int SaveChanges()
+        {
+            return this.context.SaveChanges();
+        }
+
+        private IRepository<T> GetRepository<T>() where T : class
+        {
+            if (!this.repositories.ContainsKey(typeof(T)))
+            {
+                var type = typeof(GenericRepository<T>);
+
+                this.repositories.Add(typeof(T), Activator.CreateInstance(type, this.context));
+            }
+
+            return (IRepository<T>)this.repositories[typeof(T)];
+        }
+    }
+}
---
+++ FamousQuoteQuiz.Data/IFamousQuizData.cs
@@ -0,0 +1,22 @@
+﻿using FamousQuoteQuiz.Data.Repositories;
+using FamousQuoteQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamousQuoteQuiz.Data
+{
+    public interface IFamousQuizData
+    {
+        DbContext Context { get; }
+
+        IRepository<User> Users { get; }
+
+        void Dispose();
+
+        int SaveChanges();
+    }
+}
